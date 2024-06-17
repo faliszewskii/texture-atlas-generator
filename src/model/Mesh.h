e58f2ab4f0dcbca762e03f921c1aba1de5d53d05
Igor Faliszewski <igor.faliszewski1@gmail.com>
2024-06-17 10:17:01 +0200

    Add basic data structures.

---
+++ src/model/Mesh.h
@@ -0,0 +1,13 @@
+//
+// Created by faliszewskii on 17.06.24.
+//
+
+#ifndef MATERIAL_PAINTER_MESH_H
+#define MATERIAL_PAINTER_MESH_H
+
+namespace TAGen {
+    struct Mesh {
+    };
+}
+
+#endif //MATERIAL_PAINTER_MESH_H
---
+++ src/model/Triangulation.h
@@ -0,0 +1,13 @@
+//
+// Created by faliszewskii on 17.06.24.
+//
+
+#ifndef MATERIAL_PAINTER_TRIANGULATION_H
+#define MATERIAL_PAINTER_TRIANGULATION_H
+
+namespace TAGen {
+    struct Triangulation {
+    };
+}
+
+#endif //MATERIAL_PAINTER_TRIANGULATION_H
